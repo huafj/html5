8579ac5004ea90d7f4cabbc8bb07a231ae51f403
huafj <huafeijun@gmail.com>
2020-12-18 21:59:49 +0800

    alpha

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"html/template"
 	"io"
@@ -9,23 +10,31 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 type apiObj struct {
-	Days    int    `json:"days"`
-	AccDays int    `json:"accDays,omitempty"`
-	Current int    `json:"current,omitempty"`
-	Add     int    `json:"add,omitempty"`
-	Title   string `json:"title"`
-	ID      int    `json:"id,omitempty"`
+	Days      int    `json:"days"`
+	AccDays   int    `json:"accDays,omitempty"`
+	Current   int    `json:"current,omitempty"`
+	Add       int    `json:"add,omitempty"`
+	Title     string `json:"title"`
+	Gift      string `json:"gift"`
+	ID        int    `json:"id,omitempty"`
+	canUpdate bool
 }
 
 type Server struct {
-	ID       int      `json:"id"`
-	Objs     []apiObj `json:"objs"`
-	htmlTmpl *template.Template
+	ID          int       `json:"id"`
+	Updated     time.Time `json:"updated"`
+	Objs        []apiObj  `json:"objs"`
+	DoneObjs    []apiObj  `json:"done,omitempty"`
+	TempObjs    []apiObj  `json:"-"`
+	htmlTmpl    *template.Template
+	config      string
+	forceUpdate bool
 }
 
 func (srv *Server) createObj(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
@@ -33,6 +42,7 @@ func (srv *Server) createObj(w http.ResponseWriter, r *http.Request, params http
 	obj := apiObj{}
 	json.Unmarshal(body, &obj)
 	obj.ID = srv.ID
+	obj.canUpdate = true
 	srv.ID++
 	srv.Objs = append(srv.Objs, obj)
 	fmt.Fprintf(w, string(body))
@@ -49,13 +59,15 @@ func (srv *Server) updateObj(w http.ResponseWriter, r *http.Request, params http
 	}
 	for i := range srv.Objs {
 		if srv.Objs[i].ID == obj.ID {
-			if obj.Add != 0 {
+			if obj.Add != 0 && (srv.Objs[i].canUpdate || srv.forceUpdate) {
+				srv.Objs[i].canUpdate = false
 				srv.Objs[i].Current += obj.Add
 				if srv.Objs[i].Current < 0 {
 					srv.Objs[i].Current = 0
 				}
 			}
 			srv.Objs[i].Days = obj.Days
+			srv.Objs[i].Gift = obj.Gift
 			fmt.Fprintf(w, string(body))
 			go func() {
 				fmt.Printf("update %s\n", body)
@@ -86,6 +98,7 @@ func (srv *Server) deleteObj(w http.ResponseWriter, r *http.Request, params http
 }
 
 func (srv *Server) getObjs(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	srv.TempObjs = srv.Objs
 	srv.htmlTmpl.Execute(w, srv)
 }
 
@@ -105,6 +118,31 @@ func (srv *Server) Save() {
 
 func main() {
 	srv := &Server{Objs: make([]apiObj, 0, 0)}
+	port := flag.Int("port", 80, "port")
+	flag.BoolVar(&srv.forceUpdate, "force", false, "force update")
+	flag.StringVar(&srv.config, "config", "config.json", "config file")
+	flag.Parse()
+
+	canUpdate := false
+	if srv.Updated.IsZero() {
+		canUpdate = true
+		srv.Updated = time.Now()
+		srv.ID = 0
+	} else {
+		now := time.Now()
+		if now.Sub(srv.Updated).Hours() > 23.0 {
+			canUpdate = true
+			srv.Updated = now
+		}
+	}
+
+	for i := range srv.Objs {
+		srv.Objs[i].canUpdate = canUpdate
+		if canUpdate {
+			srv.Objs[i].AccDays += 1
+		}
+	}
+
 	htmlBody, err := ioutil.ReadFile("index.html")
 	if err != nil {
 		panic(err)
@@ -118,5 +156,5 @@ func main() {
 	router.POST("/api/create", srv.createObj)
 	router.DELETE("/api/delete", srv.deleteObj)
 	router.GET("/", srv.getObjs)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), router))
 }
