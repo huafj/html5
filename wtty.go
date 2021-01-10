5e13c1151a7122f476e6e2ee5bec78d96bd08579
fhua <huafeijun@gmail.com>
2021-01-10 04:01:11 +0000

    adapter http request

--- wtty.go
+++ wtty.go
@@ -5,11 +5,35 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 
 	"golang.org/x/term"
 )
+
+type simpleTransport struct {
+	conn net.Conn
+}
+
+func (tr *simpleTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp := &http.Response{}
+	host := req.URL.Hostname()
+	port := req.URL.Port()
+	addr := net.JoinHostPort(host, port)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	tr.conn = conn
+	body := fmt.Sprintf("%s %s %s\nHost:%s\n\n", req.Method, req.URL.Path, req.Proto, addr)
+	if _, err = io.WriteString(conn, body); err != nil {
+		resp.StatusCode = http.StatusInternalServerError
+	} else {
+		resp.StatusCode = http.StatusOK
+	}
+	return resp, nil
+}
 
 func main() {
 	addr := flag.String("addr", "127.0.0.1:80", "address")
@@ -25,12 +49,15 @@ func main() {
 	if !strings.Contains(*addr, ":") || strings.HasSuffix(*addr, ":") {
 		*addr = fmt.Sprintf("%s:80", strings.Trim(*addr, ":"))
 	}
-	conn, err := net.Dial("tcp", *addr)
-	if err != nil {
+	tr := simpleTransport{}
+	client := http.Client{
+		Transport: &tr,
+	}
+	url := fmt.Sprintf("http://%s/exec/%s", *addr, *cmd)
+	if _, err := client.Get(url); err != nil {
 		panic(err)
 	}
-	body := fmt.Sprintf("GET /exec/%s HTTP/1.1\nHost:%s\n\n", *cmd, *addr)
-	io.WriteString(conn, body)
+	conn := tr.conn
 	go io.Copy(conn, os.Stdin)
 	io.Copy(os.Stdout, conn)
 }
